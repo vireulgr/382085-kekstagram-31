f79deef680051a60f384436d1d2b4d2740280de0
George G <grreulvi@gmail.com>
2024-03-17 15:07:50 +0300

    [module8-task1] добавил обработку Enter; удалил ненужные комментарии

--- js/modal-dialog.js
+++ js/modal-dialog.js
@@ -3,34 +3,45 @@ import { renderPost, clearPost } from './post';
 const dialogContainer = document.querySelector('.big-picture');
 const closeButton = dialogContainer.querySelector('#picture-cancel');
 
-// открытие диалога сделано через делегирование обработки
-// событий клика для каждого изображения
 const picturesContainer = document.querySelector('.pictures');
+// открытие диалога по клику сделано через делегирование обработки
+// событий клика для каждого изображения
 picturesContainer.addEventListener('click', (evt) => {
   if (evt.target.classList.contains('picture__img')) {
+    evt.preventDefault(); // чтобы не переходить по ссылке
     // dataset.id нужно чтобы содержимое диалога знало откуда
     // брать данные для отображения
     openDialog(evt.target.dataset.id);
   }
 });
 
-// Напишите код для закрытия окна по нажатию клавиши
-// Esc и клике по иконке закрытия.
+// открытие диалога по Enter сделано через делегирование обработки
+// событий клика для каждой ссылки
+picturesContainer.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Enter'
+    && evt.target.tagName === 'A'
+    && evt.target.classList.contains('picture')) {
+    evt.preventDefault(); // чтобы не переходить по ссылке
+    const id = evt.target.querySelector('.picture__img').dataset.id;
+    // dataset.id нужно чтобы содержимое диалога знало откуда
+    // брать данные для отображения
+    openDialog(id);
+  }
+});
+
 function closeDialog() {
   dialogContainer.classList.add('hidden');
-  // При закрытии окна не забудьте удалить этот класс.
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeyDown);
   closeButton.removeEventListener('click', onBtnCloseClicked);
   clearPost();
 }
 
+/**
+  * @param {any} data данные для внутренности диалога
+  */
 function openDialog(data) {
-  // После открытия окна добавьте тегу <body> класс modal-open,
-  // чтобы контейнер с фотографиями позади не прокручивался при скролле.
   document.body.classList.add('modal-open');
-  // Для отображения окна нужно удалять класс hidden у элемента .
-  // big-picture и каждый раз заполнять его данными о конкретной фотографии:
   dialogContainer.classList.remove('hidden');
   document.addEventListener('keydown', onDocumentKeyDown);
   closeButton.addEventListener('click', onBtnCloseClicked);
@@ -47,5 +58,4 @@ function onBtnCloseClicked(evt) {
   evt.preventDefault();
   closeDialog();
 }
-
 

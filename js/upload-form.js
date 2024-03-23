9e96c103f2bc5f70a52d5b6437635055abcfdcdc
vireulgr <grreulvi@gmail.com>
2024-03-24 01:59:22 +0300

    [module9-task1] создал модуль для формы загрузки изображения

---
+++ js/upload-form.js
@@ -0,0 +1,79 @@
+import { ModalDialog } from './utils/modal-dialog';
+import { HashTagsValidator } from './utils/validators';
+
+const uploadFormEl = document.querySelector('.img-upload__form');
+const descrInputEl = uploadFormEl.querySelector('.text__description');
+const hashInputEl = uploadFormEl.querySelector('.text__hashtags');
+
+const dialog = new ModalDialog({
+  dialogSelector: '.img-upload__overlay',
+  closeButtonSelector: '#upload-cancel',
+  renderFn: render,
+  cleanupFn: cleanup
+});
+
+const pristine = new Pristine(uploadFormEl, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'img-upload__field-wrapper--error',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'p'
+});
+
+
+const imgUploadEl = document.querySelector('.img-upload__input');
+
+imgUploadEl.addEventListener('change', () => {
+  dialog.openDialog();
+});
+
+
+function onInputKeyPressed(evt) {
+  if (evt.key === 'Escape') {
+    evt.stopPropagation();
+  }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  if (!pristine.validate()) {
+    return;
+  }
+
+  const formData = new FormData(uploadFormEl);
+  fetch('https://31.javascript.htmlacademy.pro/kekstagram', {
+    method: 'post',
+    body: formData
+  })
+    .then((resp) => resp.json())
+    .then((d) => {
+      console.log(d);
+    });
+}
+
+function render() {
+  // nouislider
+  // <div class="effect-level__slider"></div>
+  // <input class="effect-level__value" type="number" step="any" name="effect-level" value="">
+
+  descrInputEl.addEventListener('keydown', onInputKeyPressed);
+  hashInputEl.addEventListener('keydown', onInputKeyPressed);
+
+  uploadFormEl.addEventListener('submit', onFormSubmit);
+
+  pristine.addValidator(descrInputEl, () => true, 'Ошибка валидации описания');
+
+  const tagsValidator = new HashTagsValidator();
+  pristine.addValidator(
+    hashInputEl,
+    tagsValidator.process.bind(tagsValidator),
+    tagsValidator.getError.bind(tagsValidator));
+}
+
+function cleanup() {
+  imgUploadEl.value = '';
+
+  descrInputEl.removeEventListener('keydown', onInputKeyPressed);
+  hashInputEl.removeEventListener('keydown', onInputKeyPressed);
+
+  uploadFormEl.removeEventListener('submit', onFormSubmit);
+}
